9aa9c92d88600c0f193cf41fe06b61e29b3ffab0
sircatalyst <temibami@gmail.com>
2019-06-06 12:09:54 +0100

    added model with query sample tests

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 const restify = require('restify');
+const Users = require('../api/models/users');
 
 const server = restify.createServer();
 
@@ -12,5 +13,22 @@ server.listen(PORT, () => {
 });
 
 server.get('/', (req, res) => {
+    Users.byEmail('temibami@gmail.com').then(function(u) {  
+        console.log('Got user:', u.get('firstName'));
+    });
     res.send('it is working fine!');
+})
+
+
+server.post('/', (req, res) => {
+    var data = {  
+        firstName: 'Johnson',
+        lastName: 'Doe',
+        email: 'joe@example.com',
+        password: 28
+    }
+    
+    Users.forge(data).save().then(function(u) {  
+        console.log('User saved:', u.get('firstName'));
+    });
 })
---
+++ api/models/users.js
@@ -0,0 +1,30 @@
+const bookshelf = require('../knex');
+
+var Users = bookshelf.Model.extend({  
+        tableName: 'users',
+        hasTimestamps: true,
+    },
+    {
+        verifyPassword: function(password) {
+            return this.get('password') === password;
+        },
+
+        byEmail: function(email) {
+            return this.forge().query({where:{ email: email }}).fetch();
+        },
+
+        findAll: function(filter, options) {
+            return this.forge().where(filter).fetchAll(options);
+        },
+
+        findOne: function(query, options) {
+            return this.forge(query).fetch(options);
+        },
+
+        create: function(data, options) {
+            return this.forge(data).save(null, options);
+        },
+    }
+);
+
+module.exports = Users;
