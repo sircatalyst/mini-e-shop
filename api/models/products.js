d1ac04dc65a5cb70d6999059019322ef6358cff1
sircatalyst <temibami@gmail.com>
2019-06-07 14:53:31 +0100

    created products model

---
+++ api/models/products.js
@@ -0,0 +1,35 @@
+const bookshelf = require('../config/bookshelf');
+
+var Products = bookshelf.Model.extend({  
+        tableName: 'products',
+        hasTimestamps: true,
+    },
+    {
+        create: function(data, options) {
+            return this.forge(data).save(null, options);
+        },
+
+        update: function(id, data) {
+            return this.forge().where({id: id}).save(data, {method: 'update'});
+         
+        },
+
+        findOne: function(id, options) {
+            return this.forge({id: id}).fetch(options);
+        },
+
+        findByName: function(query, options) {
+            return this.forge({name: query}).fetch(options);
+        },
+
+        findAll: function(options) {
+            return this.forge().fetchAll(options);
+        },
+
+        delete: function(id) {
+            return this.forge().where(id).destroy();
+        }
+    }
+);
+
+module.exports = Products;
