eb9e9ab2034717c103351c57c7fdabff6d5ae2fb
sircatalyst <temibami@gmail.com>
2019-06-08 00:07:21 +0100

    created editCart in CartController

--- api/controllers/Cart.js
+++ api/controllers/Cart.js
@@ -12,8 +12,8 @@ const CartController = {
             product_id: req.body.product_id,
             description: req.body.description,
             price: req.body.price,
-            ordered: req.body.ordered,
             tag: req.body.tag,
+            ordered: "0",
         }
 
         const validData = Joi.validate(data, schemaCart);
@@ -29,6 +29,47 @@ const CartController = {
         }
         else {
             Cart.create(validData.value)
+            .then((cart) => {  
+                res.send(201, {
+                    status: 'success',
+                    data: cart
+                });
+                next();
+            })
+            .catch(error => {
+                res.send(500, {
+                    status: 'error',
+                    message: 'Server error',
+                    error: error
+                });
+            })
+        }
+    },
+
+    editCart (req, res, next) {
+        var data = {  
+            name: req.body.name,
+            user_id: req.user.id,
+            product_id: req.body.product_id,
+            description: req.body.description,
+            price: req.body.price,
+            tag: req.body.tag,
+            ordered: "0",
+        }
+
+        const validData = Joi.validate(data, schemaCart);
+
+        if(validData.error != null){
+            const err = validData.error.toString().replace(/Error:/, '');
+            res.send(422, {
+                status: 'error',
+                message: 'Invalid request data',
+                error: err,
+            });
+            next();
+        }
+        else {
+            Categories.update(req.params.id, validData.value)
             .then((cart) => {  
                 res.send(201, {
                     status: 'success',
