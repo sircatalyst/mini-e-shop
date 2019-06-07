56746b48c1c7569dcd7c8ca47a1c2f48e2dc2334
sircatalyst <temibami@gmail.com>
2019-06-07 23:49:49 +0100

    created and ran cart table migration

---
+++ api/migrations/20190607234239_cart.js
@@ -0,0 +1,20 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('cart', (table) => {
+        table.increments();
+        table.integer('user_id').unsigned().notNullable();
+        table.foreign('user_id').references('id').inTable('users');
+        table.integer('product_id').unsigned().notNullable();
+        table.foreign('product_id').references('id').inTable('product');
+        table.string('name').notNullable();
+        table.string('description').notNullable();
+        table.string('price').notNullable();
+        table.string('ordered').notNullable();
+        table.string('tag').notNullable();
+        //to create update_at and created_at
+        table.timestamps();
+    });  
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('cart')
+};
