968f5be060d02a3cafa62a67f47b9797ad8fa882
sircatalyst <temibami@gmail.com>
2019-06-07 13:19:32 +0100

    create CategoriesController addcategory

---
+++ api/controllers/Categories.js
@@ -0,0 +1,66 @@
+const errors = require('restify-errors');
+const Categories = require('../models/categories');
+const schemaCategories = require('../validations/schemaCategories');
+const Joi = require('joi');
+
+const CategoriesController = {
+
+    addCategory (req, res, next) {
+        var data = {  
+            name: req.body.name,
+            description: req.body.description,
+            tag: req.body.tag,
+        }
+
+        const validData = Joi.validate(data, schemaCategories);
+
+        if(validData.error != null){
+            const err = validData.error.toString().replace(/Error:/, '');
+            res.send(422, {
+                status: 'error',
+                message: 'Invalid request data',
+                error: err,
+            });
+            next();
+        }
+        else {
+            Categories.findByName(validData.value.name)
+            .then((category) => {  
+                if(category != null){
+                    res.send(401, {
+                        status: 'error',
+                        message: 'Category already exist',
+                    });
+                    next();
+                }
+                else {
+                    Categories.create(validData.value)
+                    .then((category) => {  
+                            res.send(201, {
+                                status: 'success',
+                                data: category
+                            }
+                        );
+                        next();
+                    })
+                    .catch(error => {
+                        res.send(500, {
+                            status: 'error',
+                            message: 'Server error',
+                            error: error
+                        });
+                    });
+                }
+            })
+            .catch(error => {
+                res.send(500, {
+                    status: 'error',
+                    message: 'Server error',
+                    error: error
+                });
+            })
+        }
+    },
+}
+
+module.exports = CategoriesController;
