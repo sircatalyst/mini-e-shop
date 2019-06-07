a30db21995a0885de20db3d3c92b79ef910424d1
sircatalyst <temibami@gmail.com>
2019-06-08 00:03:07 +0100

    created addToCart in CartController

---
+++ api/controllers/Cart.js
@@ -0,0 +1,50 @@
+const errors = require('restify-errors');
+const Cart = require('../models/cart');
+const schemaCart = require('../validations/schemaCart');
+const Joi = require('joi');
+
+const CartController = {
+
+    addToCart (req, res, next) {
+        var data = {  
+            name: req.body.name,
+            user_id: req.user.id,
+            product_id: req.body.product_id,
+            description: req.body.description,
+            price: req.body.price,
+            ordered: req.body.ordered,
+            tag: req.body.tag,
+        }
+
+        const validData = Joi.validate(data, schemaCart);
+
+        if(validData.error != null){
+            const err = validData.error.toString().replace(/Error:/, '');
+            res.send(422, {
+                status: 'error',
+                message: 'Invalid request data',
+                error: err,
+            });
+            next();
+        }
+        else {
+            Cart.create(validData.value)
+            .then((cart) => {  
+                res.send(201, {
+                    status: 'success',
+                    data: cart
+                });
+                next();
+            })
+            .catch(error => {
+                res.send(500, {
+                    status: 'error',
+                    message: 'Server error',
+                    error: error
+                });
+            })
+        }
+    },
+}
+
+module.exports = CartController;
