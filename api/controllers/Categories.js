2c0d35d472243bcca62913218b7a074c4fa02336
sircatalyst <temibami@gmail.com>
2019-06-07 13:21:55 +0100

    create editCategory method in categoriesController

--- api/controllers/Categories.js
+++ api/controllers/Categories.js
@@ -61,6 +61,63 @@ const CategoriesController = {
             })
         }
     },
+
+    editCategory (req, res, next) {
+        var data = {  
+            name: req.body.name,
+            description: req.body.description,
+            tag: req.body.tag,
+        }
+
+        const validData = Joi.validate(data, schemaCategories);
+
+        if(validData.error != null){
+            const err = validData.error.toString().replace(/Error:/, '');
+            res.send(422, {
+                status: 'error',
+                message: 'Invalid request data',
+                error: err,
+            });
+            next();
+        }
+        else {
+            Categories.findByName(validData.value.name)
+            .then((category) => {  
+                if(category){
+                    res.send(401, {
+                        status: 'error',
+                        message: 'Category already exist',
+                    });
+                    next();
+                }
+                else {
+                    Categories.create(validData.value)
+                    .then((category) => {  
+                            res.send(201, {
+                                status: 'success',
+                                data: category
+                            }
+                        );
+                        next();
+                    })
+                    .catch(error => {
+                        res.send(500, {
+                            status: 'error',
+                            message: 'Server error',
+                            error: error
+                        });
+                    });
+                }
+            })
+            .catch(error => {
+                res.send(500, {
+                    status: 'error',
+                    message: 'Server error',
+                    error: error
+                });
+            })
+        }
+    },
 }
 
 module.exports = CategoriesController;
