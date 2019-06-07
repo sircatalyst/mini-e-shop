5f2c4d2b4ae27a7f74553d5b62e86229cb5a98a9
sircatalyst <temibami@gmail.com>
2019-06-07 15:12:52 +0100

    created editProduct method in ProductController

--- api/controllers/Products.js
+++ api/controllers/Products.js
@@ -26,7 +26,7 @@ const ProductController = {
             next();
         }
         else {
-            Categories.findOne()
+            Categories.findOne(validData.value.categories_id)
             .then(category => {
                 if(!category) {
                     res.send(422, {
@@ -84,6 +84,88 @@ const ProductController = {
         }
     },
 
+    editProduct (req, res, next) {
+        var data = {  
+            categories_id: req.body.categories_id,
+            name: req.body.name,
+            description: req.body.description,
+            tag: req.body.tag,
+        }
+
+        const validData = Joi.validate(data, schemaCategories);
+
+        if(validData.error != null){
+            const err = validData.error.toString().replace(/Error:/, '');
+            res.send(422, {
+                status: 'error',
+                message: 'Invalid request data',
+                error: err,
+            });
+            next();
+        }
+        else {
+            Categories.findOne(validData.value.categories_id)
+            .then(category => {
+                if(!category) {
+                    res.send(422, {
+                        status: 'error',
+                        message: 'Invalid category',
+                        error: err,
+                    });
+                    next();
+                }
+                else {
+                    Product.findByName(validData.value.name)
+                    .then((product) => {  
+                        if(product){
+                            res.send(401, {
+                                status: 'error',
+                                message: 'Product already exist',
+                            });
+                            next();
+                        }
+                        else {
+                            Product.update(req.params.id, validData.value)
+                            .then((product) => {  
+                                    res.send(201, {
+                                        status: 'success',
+                                        data: product
+                                    }
+                                );
+                                next();
+                            })
+                            .catch(error => {
+                                console.log(error)
+                                res.send(500, {
+                                    status: 'error',
+                                    message: 'Server error1',
+                                    error: error
+                                });
+                            });
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error)
+                        res.send(500, {
+                            status: 'error',
+                            message: 'Server error2',
+                            error: error
+                        });
+                    })
+                }
+            })
+            .catch(error => {
+                console.log(error)
+                res.send(500, {
+                    status: 'error',
+                    message: 'Server error2',
+                    error: error
+                });
+            })
+        }
+    },
+
+ 
 
 }
 
