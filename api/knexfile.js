ed2bf0435a0e1b479168870dcb73e8f2e0ebc5a5
sircatalyst <temibami@gmail.com>
2019-06-08 17:01:26 +0100

    .env samples included

--- api/knexfile.js
+++ api/knexfile.js
@@ -1,11 +1,11 @@
 // Update with your config settings.
 require('dotenv').config();
-
-module.exports = {
+ 
+module.exports = {  
   development: {
     client: process.env.CLIENT,
     connection: {
-      host : process.env.HOST,
+      host : process.env.HOST, 
       database: process.env.DATABASE,
       user: process.env.USER,
       password: process.env.PASSWORD
